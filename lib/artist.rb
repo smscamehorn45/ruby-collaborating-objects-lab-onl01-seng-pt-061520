d205d7c01e170f34b48645063a4e6ddc411da52a
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-13 23:55:55 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []  #stores all instances of Artist 
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song    
+  end
+
+  def save
+    @@all << self     #saves all created instances of Artist class in 
+                      #@@all class variable
+  end
+
+  def self.all      #class method all returns all instances of Artist class 
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)   
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name)      variable
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.create(name)   #class method creates & stores instances vs initializing
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def print_songs #instance method iterates through @songs of an instance of Artist
+    self.songs.each {|song| song.name}
+  end
+end
